2dd691c6c2612e0c8bd841caabce81fd6f0ace6b
bentto90 <cch3056@gmail.com>
2023-04-10 18:42:07 -0700

    /API KEY FIXED

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const sequelize = require('../config/connection');
 const axios = require('axios');
-const data = require('../config/data');
+const data = require('../server.js');
 const { User} = require('../models');
 
 const userData = require('./userData.json');
@@ -25,7 +25,7 @@ class Latest {
     }
     async getDate() {
         try {
-            const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${data.key}&language=pl-PL&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&primary_release_year=${this.releaseYear}&primary_release_date.lte=${this.releasedTillNow}&vote_count.gte=8&with_genres=27`);
+            const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${data.API_KEY}&language=pl-PL&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&primary_release_year=${this.releaseYear}&primary_release_date.lte=${this.releasedTillNow}&vote_count.gte=8&with_genres=27`);
             this.result = response.data.results;
         } catch (err) {
             console.log("Error: " + err);
