6498b9c94bb2b52ab77dce6ebbc8d607d8c326ce
bentto90 <cch3056@gmail.com>
2023-04-12 14:05:11 -0700

    /home route changed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const { Movie } = require('./movieControllers.js');
 
 // renders homepage 
 router.get('/', async (req, res) => {
-    res.render('homepage');
+    try {
+        const movies = await Movie.findALL();
+        res.render('homepage', { movies });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+   
 });
 
 // renders about page
