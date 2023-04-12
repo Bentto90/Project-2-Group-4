cc0d9534a6eace8122491fdfe5ca3b4034214bad
bentto90 <cch3056@gmail.com>
2023-04-11 20:35:59 -0700

    /re-structured mvc to make it work

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const movieController = require('../controllers/index.js');
 
-const indexRouter = require('./indexRouter.js');
-const searchRouter = require('./searchRouter.js');
-const movieRankRouter = require('./movieRankRouter.js');
-const movieDetailRouter = require('./movieDetailRouter.js');
-
-router.use('/', indexRouter);
-router.use('/search', searchRouter);
-router.use('/movieRank', movieRankRouter);
-router.use('/movie/:movieId', function (req, res, next) {
-    movieId = req.params.movieId;
-    next();
-}, movieDetailRouter);
+router.get('/', movieController.getTrendingMovies);
+router.get('/movie/:id', movieController.getMovieDetails);
 
 module.exports = router;
