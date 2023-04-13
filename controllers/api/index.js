1decb7e17ae9aa50e50463eb8764cef60d2b9082
Antonio <robledoy@uci.edu>
2023-04-12 19:06:39 -0700

    worked on user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const homeRoutes = require('../homeRoutes');
+const userRoutes = require('./userRoutes')
 
-router.use('/', homeRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
 
