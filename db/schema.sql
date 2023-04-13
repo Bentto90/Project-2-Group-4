07f1504deb8362eb93932c0ef98681e01c50d23e
Antonio <robledoy@uci.edu>
2023-04-13 13:22:40 -0700

    added partials folder

--- db/schema.sql
+++ db/schema.sql
@@ -9,13 +9,13 @@ CREATE DATABASE horrormovies_db;
      email VARCHAR(30) NOT NULL,
      password VARCHAR(200) NOT NULL
  );
-/*
+
  CREATE TABLE review(
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
    rating INT NOT NULL,
    content VARCHAR(300) NOT NULL
    
- ) */
+ )
   CREATE TABLE watchlist (
      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     user_id INT NOT NULL,
