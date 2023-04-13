5fb222fac9f7de98f1134382b1607827ffe17490
bentto90 <cch3056@gmail.com>
2023-04-13 14:02:31 -0700

    /merge conflict fixed

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('./models');
+const { User } = require('../../models');
 
 // router.get("/", (req, res) => {
 //   // Access our User model and run .findAll() method
