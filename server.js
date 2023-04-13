6545bcb151b76b19b85642849aa1cea534fb6621
bentto90 <cch3056@gmail.com>
2023-04-13 15:00:36 -0700

    /port in server.js changed

--- server.js
+++ server.js
@@ -117,7 +117,6 @@ app.use(routes);
   })
 
 
-  app.listen(PORT, () => 
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
-
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
